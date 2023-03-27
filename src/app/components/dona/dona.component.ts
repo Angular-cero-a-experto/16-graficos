e2cffa0b605fb938c09113a7392831e5b5796be1
pachecov <pachecochoque@gmail.com>
2023-03-27 10:59:52 -0400

    first commit

---
+++ src/app/components/dona/dona.component.ts
@@ -0,0 +1,44 @@
+import { Component, ViewChild } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styleUrls: ['./dona.component.css']
+})
+export class DonaComponent {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ] },
+      // { data: [ 50, 150, 120 ] },
+      // { data: [ 250, 130, 70 ] }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  numerosRandom() {
+
+    this.doughnutChartData.datasets[0].data = [
+      Math.random()*100,
+      Math.random()*100,
+      Math.random()*100,
+    ]
+
+    this.chart?.update();
+    console.log('ddddddddddddddddd')
+  }
+}
